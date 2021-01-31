de0a8c98076f522f796863ca713dcdb438ef5bb1
My First Commit <marcogabriele81@users.noreply.github.com>
2021-01-31 11:20:44 -0800

    Update contactme.php

--- contactme.php
+++ contactme.php
@@ -20,8 +20,8 @@ $message = strip_tags(htmlspecialchars($_POST['message']));
 // Create the email and send the message
 $to = 'gabriele.marco@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Contacto desde la web de parte de:  $name";
-$email_body = "Hola Verónica, recibiste un mensaje desde la planilla de contacto.\n\n"."Estos son los detalles:\n\nNombre: $name\n\nEmpresa: $company\n\nEmail: $email_address\n\nTeléfono: $phone\n\nPaís: $country\n\nConsulta:\n$message";
-$headers = "From: noreply@marcogabriele81.github.io/Travel-During-Covid-World/\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$email_body = "Hola Verónica, recibiste un mensaje desde la planilla de contacto.\n\n"."Estos son los detalles:\n\nNombre: $name\n\nEmail: $email_address\n\nTeléfono: $phone\n\nSomething:\n$message";
+$headers = "From: gabriele.marco@gmail.com/\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";   
 
 $result = mail($to,$email_subject,$email_body,$headers);
