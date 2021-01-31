bb92ee0d94d0d49a864224313209762055654b68
My First Commit <marcogabriele81@users.noreply.github.com>
2021-01-31 11:15:09 -0800

    Update contactme.php

--- contactme.php
+++ contactme.php
@@ -1,1 +1,29 @@
-the form again."; } else{ $from="From: $name<$email>\r\nReturn-path: $email"; $subject="Message sent using your contact form"; mail("gabriele.marco@gmail.com", $subject, $message, $from); echo "Email sent!"; } } ?>
+<?php
+// Check for empty fields
+if(empty($_POST['name'])      ||
+   empty($_POST['email'])     ||
+   empty($_POST['phone'])     ||
+   empty($_POST['text'])     ||
+   empty($_POST['message'])   ||
+   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+{
+   echo "No arguments Provided!";
+   return false;
+}
+   
+$name = strip_tags(htmlspecialchars($_POST['name']));
+$email_address = strip_tags(htmlspecialchars($_POST['email']));
+$phone = strip_tags(htmlspecialchars($_POST['phone']));
+$country = strip_tags(htmlspecialchars($_POST['text']));
+$message = strip_tags(htmlspecialchars($_POST['message']));
+   
+// Create the email and send the message
+$to = 'gabriele.marco@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "Contacto desde la web de parte de:  $name";
+$email_body = "Hola Verónica, recibiste un mensaje desde la planilla de contacto.\n\n"."Estos son los detalles:\n\nNombre: $name\n\nEmpresa: $company\n\nEmail: $email_address\n\nTeléfono: $phone\n\nPaís: $country\n\nConsulta:\n$message";
+$headers = "From: noreply@marcogabriele81.github.io/Travel-During-Covid-World/\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers .= "Reply-To: $email_address";   
+
+$result = mail($to,$email_subject,$email_body,$headers);
+die($result);         
+?>
