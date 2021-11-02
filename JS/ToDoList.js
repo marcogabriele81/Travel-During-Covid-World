8672533eb0be346f32dd31535b89afaa85d322b1
My First Commit <marcogabriele81@users.noreply.github.com>
2021-11-01 22:22:00 -0700

    lightbox completed

---
+++ JS/ToDoList.js
@@ -0,0 +1,68 @@
+// let ourForm = document.getElementById("ourForm");
+// let ourField = document.getElementById("ourField");
+// let ourList = document.getElementById("ourList")
+// ourForm.addEventListener("submit", (e) => {
+//     e.preventDefault();
+//     createItem(ourField.value);
+// })
+
+// function createItem(x){
+//     let ourHTML = `<li>${x} <button>Delete</button></li>`
+//     ourList.insertAdjacentHTML("beforeend", ourHTML)
+//     ourField.value = ""
+
+// Create a "close" button and append it to each list item
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
+// Click on a close button to hide the current list item
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+// Add a "checked" symbol when clicking on a list item
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+// Create a new list item when clicking on the "Add" button
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
--- JS/main.js
+++ JS/main.js
@@ -4,3 +4,108 @@ function changeimage()
 document.getElementById("onclick").src="Resources/Images/covid19masked.png"
 }
 
+
+// // // Lightbox Gallery 
+
+
+//     lightbox.option({
+//       'resizeDuration': 200,
+//       'wrapAround': true
+//       'alwaysShowNavOnTouchDevices' : false	
+//     })
+
+
+// // var slideIndex = 1;
+// // showSlide(slideIndex);
+
+// // function openLightbox() {
+// //   document.getElementById('Lightbox').style.display = 'block';
+// // }
+
+// // function closeLightbox() {
+// //   document.getElementById('Lightbox').style.display = 'none';
+// // }
+
+// // function changeSlide(n) {
+// // 	showSlide(slideIndex += n);
+// // }
+
+// // function toSlide(n) {
+// // 	showSlide(slideIndex = n);
+// // }
+
+// // function showSlide(n) {
+
+// //   const slides = document.getElementsByClassName('slide');
+// //   let modalPreviews = document.getElementsByClassName('modal-preview');
+
+// //   if (n > slides.length) {
+// //     slideIndex = 1;	
+// //   }
+  
+// //   if (n < 1) {
+// //   	slideIndex = slides.length;
+// //   }
+
+// //   for (let i = 0; i < slides.length; i++) {
+// //       slides[i].style.display = "none";
+// //   }
+// //   for (let i = 0; i < modalPreviews.length; i++) {
+// //       modalPreviews[i].className = modalPreviews[i].className.replace(' active', '');
+// //   }
+  
+// //   slides[slideIndex - 1].style.display = 'block';
+// //   modalPreviews[slideIndex - 1].className += ' active';
+// // }
+
+// $(function() {
+//     $('#activator').click(function(){
+//         $('#overlay').fadeIn(200,function(){
+//             $('#box').animate({'top':'20px'},200);
+//         });
+//         return false;
+//     });
+//     $('#boxclose').click(function(){
+//         $('#box').animate({'top':'-200px'},500,function(){
+//             $('#overlay').fadeOut('fast');
+//         });
+//     });
+ 
+// });
+
+function openModal() {
+    document.getElementById("myModal").style.display = "block";
+  }
+  
+  function closeModal() {
+    document.getElementById("myModal").style.display = "none";
+  }
+  
+  var slideIndex = 1;
+  showSlides(slideIndex);
+  
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+  
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+  
+  function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("demo");
+    var captionText = document.getElementById("caption");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+    captionText.innerHTML = dots[slideIndex-1].alt;
+  }
